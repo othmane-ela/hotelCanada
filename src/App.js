7b9d631bb46890784c18342d1078c9a4a9a70e04
Othmane El Ahmadi <contact@othmanelahmadi.com>
2021-04-12 00:06:22 +0000

    staging 1.9

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
+
 /* COMPONNENTS AND PAGES */
 import NoMatch from "./NoMatch"
 import Navbar from './components/navbar/Navbar'
@@ -15,15 +16,15 @@ import './App.css'
 function App(props) {
     return (
         <>
-        <Router>
+        <Router >
              <Navbar/>
              <Switch>
-                 <Route path={process.env.PUBLIC_URL + "/"} exact component={HomeSlide}/>     
-                 <Route path={process.env.PUBLIC_URL +"/room"} exact component={RoomSlide} />
-                 <Route path={process.env.PUBLIC_URL +"/suite"} exact component={SuiteSlide} />
-                 <Route path={process.env.PUBLIC_URL +"/restaurant"} exact component={RestaurantSlide} />
-                 <Route path={process.env.PUBLIC_URL +"/contact"} exact component={Contact} />
-                 <Route path={process.env.PUBLIC_URL +"*"}>
+                 <Route path={`${process.env.PUBLIC_URL}/`} exact component={HomeSlide}/>     
+                 <Route path={`${process.env.PUBLIC_URL}/room`} exact component={RoomSlide} />
+                 <Route path={`${process.env.PUBLIC_URL}/suite`} exact component={SuiteSlide} />
+                 <Route path={`${process.env.PUBLIC_URL}/restaurant`} exact component={RestaurantSlide} />
+                 <Route path={`${process.env.PUBLIC_URL}/contact`} exact component={Contact} />
+                 <Route path={`${process.env.PUBLIC_URL}/*`}>
                     <NoMatch />
                 </Route>
              </Switch>
